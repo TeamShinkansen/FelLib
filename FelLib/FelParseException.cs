6ed97358e24fb6a3cb90749c39a3ca2e280777df
Daniel <790119+DanTheMan827@users.noreply.github.com>
2018-10-12 12:26:50 -0500

    Convert to library class

--- FelLib/FelParseException.cs
+++ FelLib/FelParseException.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace com.clusterrr.FelLib
+namespace FelLib
 {
     public class FelParseException : Exception
     {
